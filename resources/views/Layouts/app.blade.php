11c86f6bd52b2d97f4c529a4066d31906d4ea019
r3dfoxx <jekagrabar@gmail.com>
2020-07-29 18:46:12 +0300

    modified 2 files

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title>Donation@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
